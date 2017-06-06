145fbf9bcec4fe0f69cd464fe7bf7529ce18c5b9
Aaron Taylor <ataylor@subgeniuskitty.com>
2017-06-05 23:33:54 -0700

    "Delete Records" page now has basic functionality.

--- index.py
+++ index.py
@@ -19,9 +19,9 @@ cur = db.cursor()
 ########################################################################################################################
 
 def print_all_records(table, cursor):
-	cursor.execute("SELECT deleted,id,guest_id,facility_id,notes FROM " + config.table)
+	cursor.execute("SELECT deleted,id,guest_id,facility_id,notes FROM " + table)
 	print("<table>")
-	print("<tr><th>id</th><th>guest_id</th><th>facility_id</th><th>notes</th></tr>")
+	print("<tr><th>record_id</th><th>guest_id</th><th>facility_id</th><th>notes</th></tr>")
 	for row in cursor.fetchall():
 		if row[0] == 1:
 			print("<tr bgcolor=\"#FF0000\">")
@@ -29,6 +29,17 @@ def print_all_records(table, cursor):
 			print("<tr>")
 		print("<td>" + str(row[1]) + "</td><td>" + str(row[2]) + "</td><td>" + str(row[3]) + "</td><td>" + row[4] + "</td></tr>")
 	print("</table>")
+
+def print_delete_form():
+	print("<form method=\"post\" action=\"?mode=delete_record_execute\">")
+	print("Record ID:<br />")
+	print("<input type=\"text\" name=\"record_id\" /><br />")
+	print("<input type=\"submit\" value=\"Submit\" />")
+	print("</form>")
+
+def delete_record(table, cursor, database, record_id):
+	cursor.execute("UPDATE " + table + " SET deleted=1,deleted_timestamp=CURRENT_TIMESTAMP WHERE id=" + record_id)
+	database.commit()
 
 ########################################################################################################################
 # Main program
@@ -47,12 +58,20 @@ print("<a href=\"?mode=view\">View Records</a><br />")
 print("<a href=\"?mode=delete_record\">Delete Records</a><br />")
 
 # Branch based on user page choice
-if( mode == "view" ):
+if(mode == "view"):
 	print("<h1>All Records</h1>")
 	print_all_records(config.table, cur)
-elif( mode == "delete_record" ):
-	print("test2")
+elif(mode == "delete_record"):
+	print("<h1>Delete Records</h1>")
+	print_delete_form()
+elif(mode == "delete_record_execute"):
+	print("<h1>Delete Records</h1>")
+	delete_record(config.table, cur, db, arguments["record_id"].value)
+	print("Delete instruction issued for record_id=" + str(arguments["record_id"].value) + ". Use navigation bar to continue.<br />")
 else:
 	print("<h1>Main Menu</h1>\n")
 	# print instructions, list of facility_id -> facility names and other fields until dropdowns implemented
 	# print form to enter new record
+
+cur.close()
+db.close()
