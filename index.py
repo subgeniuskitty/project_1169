bd1df10be4989b811c353c51df6ed97891db0896
Aaron Taylor <ataylor@subgeniuskitty.com>
2017-06-05 21:45:34 -0700

    Basic page navigation is operational via POST contents

--- index.py
+++ index.py
@@ -23,23 +23,23 @@ cur = db.cursor()
 ########################################################################################################################
 
 arguments = cgi.FieldStorage()
-# We assume the user is authenticated and trusted at this point, so encourage functionality.
+# We assume the user is authenticated and trusted at this point, so encourage functionality by falling through to menu.
 if "mode" not in arguments:
 	mode = "main"
 else:
-	mode = arguments["mode"]
+	mode = arguments["mode"].value
 
+# Print the navigation bar
+print("<a href=\"?mode=main\">Enter Records</a><br />")
+print("<a href=\"?mode=view\">View Records</a><br />")
+print("<a href=\"?mode=delete_record\">Delete Records</a><br />")
+
+# Branch based on user page choice
 if( mode == "view" ):
-	# print all the data, kept separate since it will slow down as table size inceases and I want input to be snappy.
 	print("test")
-if( mode == "delete_record" ):
-	# form to delete record, kept on separate page to avoid accidents
-	print("test")
+elif( mode == "delete_record" ):
+	print("test2")
 else:
-	# We assume the user has already been validated elsewhere and has full access, so we want to encourage functionality.
-	# Thus, we fall through to the main menu.
 	print("<h1>Main Menu</h1>\n")
 	# print instructions, list of facility_id -> facility names and other fields until dropdowns implemented
-	# print link to view data
-	# print link to delete record
 	# print form to enter new record
